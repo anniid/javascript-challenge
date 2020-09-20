cc81874d04c3a94528658ae1131de0d415220276
Annelise Cope <65578617+anniid@users.noreply.github.com>
2020-09-20 10:11:04 -0500

    building the table

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -1,4 +1,31 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// get table ref, make sure it is blank first, so that all the data is new instead of added to itself repeatedly
+var tbody = d3.select("tbody");
+
+function buildT(data) {
+    tbody.html(""); //cleared
+    // for each loop through the data, append a row and cells
+    data.forEach((drow) => {
+        var row = tbody.append("tr");
+        Object.values(drow).forEach((dcell)=> {
+            var cell = row.append("td");
+            cell.text(dcell);
+        });
+    });
+}
+buildT(tableData)
+//get datetime value from the filter
+
+// was the date entered? filter data using that date
+// if date then
+    // apply filter to only keep data rows that match the input
+
+    // then rebuild the table using that filter
+
+
+//attach an event to listen for the form button
+
+//build the table when the page loads
+
