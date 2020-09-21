503bbc959f1e98674b09d0a2a85e1ac5dd75340e
Annelise Cope <65578617+anniid@users.noreply.github.com>
2020-09-20 22:20:55 -0500

    bug fixing

--- UFO-level-2/app.js
+++ UFO-level-2/app.js
@@ -9,33 +9,34 @@ function buildT(data) {
     // for each loop through the data, append a row and cells
     data.forEach((drow) => {
         var row = tbody.append("tr");
-        Object.values(drow).forEach((dcell)=> {
+        Object.values(drow).forEach((dcell) => {
             var cell = row.append("td");
             cell.text(dcell);
         });
     });
 }
 var filters = {}
-function updateFilters(){
+function updateFilters() {
     d3.event.preventDefault(); //don't refresh when click
 
-//element, value, id of filter
+    //element, value, id of filter
     var newEl = d3.select(this).select("input");
     var elVal = newEl.property("value");
     var filtID = newEl.attr("id");
-// if filter val changed, add the ID and val to the filters list. else clear the filter from the filters list
+    // if filter val changed, add the ID and val to the filters list. else clear the filter from the filters list
     if (elVal) {
-        filters[filtID] = elVal;}
+        filters[filtID] = elVal;
+    }
     else {
         delete filters[filtID]
     }
     filterTable();
-       
+}
 function filterTable() {
     let filtData = tableData;
     //loop through filters keeping data matching current values
-    Object.entries(filters)forEach(([attr,value]) => {
-    filtData = filtData.filter(row=>row[attr]===value)
+    Object.entries(filters).forEach(([attr, value]) => {
+        filtData = filtData.filter(row => row[attr] === value)
     });
     buildT(filtData); //rebuild table
 }
